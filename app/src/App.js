69149144e59a0141688fce913032cc4407b7bea2
Matheus Massari <massari.matheus@gmail.com>
2021-09-20 23:33:03 -0300

    Criacao do repositoriom primeiro commit

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class='App'>
+      <h1>
+        Alou alou alou, testando...
+      </h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, animi?</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique necessitatibus adipisci aliquam reprehenderit qui maiores unde consectetur, vitae enim impedit amet delectus exercitationem libero distinctio porro, iure expedita debitis ea!
+      </p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo earum quia aliquam? Corporis id neque quisquam quae eos cupiditate nulla debitis reiciendis, velit fugiat nostrum sed, nemo mollitia deserunt, pariatur quibusdam consequuntur. Non voluptates fugiat porro quo! Repellendus hic ratione, facere quisquam officiis at natus iure sequi numquam quae voluptatibus.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
